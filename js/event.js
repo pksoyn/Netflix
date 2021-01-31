1d262725b39eae3c337b570ae950149033929176
pksoyn <76178016+pksoyn@users.noreply.github.com>
2021-01-31 19:30:30 +0900

    slide update

--- js/event.js
+++ js/event.js
@@ -1,5 +1,10 @@
 
-const icon = document.querySelectorAll("b");
+const left_btn = document.getElementsByClassName("slider__layer__left");
+const right_btn = document.getElementsByClassName("slider__layer__right");
+const left_btn2 = document.getElementsByClassName("slider__layer__left2");
+const right_btn2 = document.getElementsByClassName("slider__layer__right2");
+const left_btn3 = document.getElementsByClassName("slider__layer__left3");
+const right_btn3 = document.getElementsByClassName("slider__layer__right3");
 const content = document.querySelector(".slider__layer");
 const content2 = document.querySelector(".slider__layer2");
 const content3 = document.querySelector(".slider__layer3");
@@ -9,31 +14,136 @@ const layer = document.getElementsByClassName("slider__layer_row_item");
 const currentindex = 0;
 
 
-function clickbtn(event){
-    
-    move();
-    icon[i].style.color="red";
+function right_clickbtn(event){
+    rightmove();
+    /*var getId=$(this).attr("id");
+    left_btn[i].style.color="red";*/
 }
-function move(){
+function left_clickbtn(event){
+    leftmove();
+}
+function right_clickbtn2(event){
+    rightmove2();
+    /*var getId=$(this).attr("id");
+    left_btn[i].style.color="red";*/
+}
+function left_clickbtn2(event){
+    leftmove2();
+}
+function right_clickbtn3(event){
+    rightmove3();
+    /*var getId=$(this).attr("id");
+    left_btn[i].style.color="red";*/
+}
+function left_clickbtn3(event){
+    leftmove3();
+}
+function rightmove(){
     var le=0;
     var i=0;
     var licounter = layer.length;
+    
     if(i<licounter-1){
-        le+=100;
+        le+=10;
         i++;
     }
     else{
         le=0;
         i=0;
     }
-    content.style
     content.style.left = "-"+le+"%";
-}
+    content.style.transition= "left 1s";
 
-function init(){
-    for(var i=0;i<icon.length;i++){
-        icon[i].addEventListener("click", clickbtn);
+}
+function leftmove(){
+    var le=0;
+    var i=0;
+    var licounter = layer.length;
+    if(i<licounter-1){
+        le+=10;
+        i++;
     }
+    else{
+        le=0;
+        i=0;
+    }
+    content.style.left = le+"%";
+    content.style.transition= "left 1s";
+}
+function rightmove2(){
+    var le=0;
+    var i=0;
+    var licounter = layer.length;
+    
+    if(i<licounter-1){
+        le+=10;
+        i++;
+        
+    content2.style.left = "-"+le+"%";
+    content2.style.transition= "left 1s";
+    }
+    else{
+        le=0;
+        i=0;
+        content2.style.transform = "translate3d(0px,0px,0px)";
+        content2.style.transition= "left 1s";
+    }
+
+}
+function leftmove2(){
+    var le=0;
+    var i=0;
+    var licounter = layer.length;
+    if(i<licounter-1){
+        le+=10;
+        i++;
+    }
+    else{
+        le=0;
+        i=0;
+    }
+    content2.style.left = le+"%";
+    content2.style.transition= "left 1s";
+}
+function rightmove3(){
+    var le=0;
+    var i=0;
+    var licounter = layer.length;
+    
+    if(i<licounter-1){
+        le+=10;
+        i++;
+    }
+    else{
+        le=0;
+        i=0;
+    }
+    content3.style.left = "-"+le+"%";
+    content3.style.transition= "left 1s";
+
+}
+function leftmove3(){
+    var le=0;
+    var i=0;
+    var licounter = layer.length;
+    if(i<licounter-1){
+        le+=10;
+        i++;
+    }
+    else{
+        le=0;
+        i=0;
+    }
+    content3.style.left = le+"%";
+    content3.style.transition= "left 1s";
+}
+function init(){
+    left_btn[0].addEventListener("click", left_clickbtn);
+    right_btn[0].addEventListener("click", right_clickbtn);
+    left_btn2[0].addEventListener("click", left_clickbtn2);
+    right_btn2[0].addEventListener("click", right_clickbtn2);
+    left_btn3[0].addEventListener("click", left_clickbtn3);
+    right_btn3[0].addEventListener("click", right_clickbtn3);
 }
 init();
 
